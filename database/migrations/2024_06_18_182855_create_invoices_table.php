29ca2a0d9b3331eb919ce8456149e358ffabe37f
Josias Silva <josilva1233@gmail.com>
2024-06-18 16:28:43 -0300

    Banco de dados com Migrations e Factories

---
+++ database/migrations/2024_06_18_182855_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('type');
+            $table->boolean('paid');
+            $table->decimal('value', total:10, places:2);
+            $table->dateTime('payment_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
